a6ca97d8eca5f558aa737664bb64bfd4de1bf2bf
Allen Wu <lisivo@gmail.com>
2015-11-14 13:35:01 +0800

    add line and fb share function
    增加Line和FB分享商品功能

--- MorePromotions.php
+++ MorePromotions.php
@@ -1,3 +1,7 @@
+<head>
+<meta property="og:title" content="The Rock"/> <!-- 分享的TITLE  -->
+<meta property="og:description" content="movie"/> <!-- 分享的內容描述 -->
+</head>
 <div id="MorePromotions" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
@@ -9,9 +13,14 @@
       </div>
       <div class="modal-body">
       <form class=form-signin role=form><h2 class=form-signin-heading>請您的朋友幫您忙,得到更優惠價格!</h2>
-<label>Line:</label> 
-<label>FB:</label> 
-<button class="btn btn-lg btn-success btn-block" type=submit>幫我一把吧!</button>
+
+<span>
+<a href="http://line.naver.jp/R/msg/text/?哈囉,我想買這個商品,請幫我取得優惠,謝謝!!%0D%0Ahttp://www.medeal.tk/product.php">
+
+<img src="http://www.medeal.tk/linebutton_84x20_zh-hant.png"></a>
+</span>
+<a type="button_count" name="fb_share" href="http://www.facebook.com/sharer.php">分享</a>
+<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
 
 </form>
       </div>
